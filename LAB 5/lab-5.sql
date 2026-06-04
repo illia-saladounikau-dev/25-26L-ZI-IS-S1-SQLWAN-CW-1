228a90a2f4386058ca4a494f43364f8b6e22842c
Illia Saladounikau <226360@student.uek.krakow.pl>
2026-06-04 20:35:46 +0200

    add exported db in  .dacpac format for LAB 5, zadanie 1 for lab 5, gitignore file

---
+++ LAB 5/lab-5.sql
@@ -0,0 +1,15 @@
+-- Illia
+-- Saladounikau
+-- 226360
+-- =============================================
+-- =============================================
+-- Zadanie 1
+-- =============================================
+
+-- Aby zbudować musiałem zmienić schemat z SalesLT na 226360 w funkcji [ufnGetCustomerInformation]. Link do repo z plikiem .dacpac (plik znajduje się w folderze 'LAB 5'):
+-- https://github.com/illia-saladounikau-dev/25-26L-ZI-IS-S1-SQLWAN-CW-1
+-- (git@github.com:illia-saladounikau-dev/25-26L-ZI-IS-S1-SQLWAN-CW-1.git / https://github.com/illia-saladounikau-dev/25-26L-ZI-IS-S1-SQLWAN-CW-1.git)
+
+-- =============================================
+-- Zadanie 2
+-- =============================================
